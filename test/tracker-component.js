c42d391a264f43f7c78d326486578357797c3404
Bryan Jennings <brjennings@paypal.com>
2019-03-12 12:43:14 -0700

    Test that appendChild is called when using tracker.view

--- test/tracker-component.js
+++ test/tracker-component.js
@@ -1,11 +1,22 @@
 /* @flow */
-/* globals describe */
+/* globals describe before it */
 
 import { expect } from 'chai';
 
 import { Tracker } from '../src/tracker-component';
 
 describe('paypal.Tracker', () => {
+    let appendChildCalls = 0;
+    const appendChild = () => {
+        appendChildCalls += 1;
+    };
+    before(() => {
+        // $FlowFixMe
+        document.body = document.body || {};
+        // $FlowFixMe
+        document.body.appendChild = appendChild;
+    });
+
     it('should be a function that returns a tracker', () => {
         const userID = '__test__userID';
         const userName = '__test__userName';
@@ -16,6 +27,15 @@ describe('paypal.Tracker', () => {
         expect(tracker).to.have.property('removeFromCart');
         expect(tracker).to.have.property('purchase');
         expect(tracker).to.not.have.property('purchases');
+    });
+
+    it('should send view events', () => {
+        const userID = '__test__userID2';
+        const userName = '__test__userName2';
+        const tracker = Tracker({ userID, userName });
+        expect(appendChildCalls).to.equal(0);
+        tracker.view({ pageUrl: 'https://example.com/test' });
+        expect(appendChildCalls).to.equal(1);
     });
 });
 
