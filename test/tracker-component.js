69cae9dc71ef586bbc225638eda792ad50eee413
Bryan Jennings <brjennings@paypal.com>
2019-03-12 13:06:09 -0700

    Add setCart test

--- test/tracker-component.js
+++ test/tracker-component.js
@@ -62,4 +62,21 @@ describe('paypal.Tracker', () => {
         expect(appendChildCalls).to.equal(2);
         return testEndToEnd ? imgLoadPromise : undefined;
     });
+
+    it('should send setCart events', () => {
+        const userID = '__test__userID3';
+        const userName = '__test__userName3';
+        const tracker = Tracker({ userID, userName });
+        expect(appendChildCalls).to.equal(2);
+        const imgLoadPromise = tracker.setCart({
+            cartId:          '__test__cartId',
+            items:           [ { id: '__test__productId' } ],
+            emailCampaignId: '__test__emailCampaignId',
+            price:           12345.67,
+            currencyCode:    'USD',
+            keywords:        [ '__test__' ]
+        });
+        expect(appendChildCalls).to.equal(3);
+        return testEndToEnd ? imgLoadPromise : undefined;
+    });
 });
