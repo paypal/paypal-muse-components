724eebfb47ec5133c75d8304fa0510be976ec00d
Bryan Jennings <brjennings@paypal.com>
2019-03-12 12:13:04 -0700

    Add tests to verify Tracker has properties

--- test/tracker-component.js
+++ test/tracker-component.js
@@ -3,9 +3,17 @@
 
 import { expect } from 'chai';
 
+import { Tracker } from '../src/tracker-component';
+
 describe('paypal tracker', () => {
-    it('should pass this test', () => {
-        expect(1 + 1).to.equal(2);
+    it('should have properties', () => {
+        expect(Tracker).to.have.property('setUser');
+        expect(Tracker).to.have.property('view');
+        expect(Tracker).to.have.property('addToCart');
+        expect(Tracker).to.have.property('setCart');
+        expect(Tracker).to.have.property('removeFromCart');
+        expect(Tracker).to.have.property('purchase');
+        expect(Tracker).to.not.have.property('purchases');
     });
 });
 
