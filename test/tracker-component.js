d5ac43d755c7412dc4ca85a11f46a91bfe0f9232
Bryan Jennings <brjennings@paypal.com>
2019-03-12 12:07:47 -0700

    Add dummy test for tracker-component

---
+++ test/tracker-component.js
@@ -0,0 +1,11 @@
+/* @flow */
+/* globals describe */
+
+import { expect } from 'chai';
+
+describe('paypal tracker', () => {
+    it('should pass this test', () => {
+        expect(1 + 1).to.equal(2);
+    });
+});
+
