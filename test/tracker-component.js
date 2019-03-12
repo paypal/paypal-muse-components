d9985cff7021bd319ba47266828d8c966f44d8ea
Bryan Jennings <brjennings@paypal.com>
2019-03-12 13:03:58 -0700

    Add addToCart tests

--- test/tracker-component.js
+++ test/tracker-component.js
@@ -5,6 +5,11 @@ import { expect } from 'chai';
 
 import { Tracker } from '../src/tracker-component';
 
+// We probably don't want to be sending events to Keen all the time so I'm
+// adding this boolean here which will make it so we only send events to Keen
+// if it's set to true.
+const testEndToEnd = false;
+
 describe('paypal.Tracker', () => {
     let appendChildCalls = 0;
     const appendChild = () => {
@@ -34,8 +39,27 @@ describe('paypal.Tracker', () => {
         const userName = '__test__userName2';
         const tracker = Tracker({ userID, userName });
         expect(appendChildCalls).to.equal(0);
-        tracker.view({ pageUrl: 'https://example.com/test' });
+        const imgLoadPromise = tracker.view({
+            pageUrl: 'https://example.com/test2'
+        });
         expect(appendChildCalls).to.equal(1);
+        return testEndToEnd ? imgLoadPromise : undefined;
+    });
+
+    it('should send addToCart events', () => {
+        const userID = '__test__userID3';
+        const userName = '__test__userName3';
+        const tracker = Tracker({ userID, userName });
+        expect(appendChildCalls).to.equal(1);
+        const imgLoadPromise = tracker.addToCart({
+            cartId:          '__test__cartId',
+            items:           [ { id: '__test__productId' } ],
+            emailCampaignId: '__test__emailCampaignId',
+            price:           12345.67,
+            currencyCode:    'USD',
+            keywords:        [ '__test__' ]
+        });
+        expect(appendChildCalls).to.equal(2);
+        return testEndToEnd ? imgLoadPromise : undefined;
     });
 });
-
