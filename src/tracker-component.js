1872f433c081ab016410fb950272242fecc6be94
Bryan Jennings <brjennings@paypal.com>
2019-03-15 16:01:38 -0700

    Remove all promises

--- src/tracker-component.js
+++ src/tracker-component.js
@@ -48,40 +48,36 @@ type Config = {|
     paramsToBeaconUrl? : ParamsToBeaconUrl
 |};
 
-const track = <T>(config : Config, trackingType : TrackingType, trackingData : T) : Promise<void> => {
+const track = <T>(config : Config, trackingType : TrackingType, trackingData : T) => {
     const encodeData = data => encodeURIComponent(btoa(JSON.stringify(data)));
 
-    return new Promise((resolve, reject) => {
-        const img = document.createElement('img');
-        img.style.display = 'none';
+    const img = document.createElement('img');
+    img.style.display = 'none';
 
-        const data = {
-            ...trackingData,
-            user:       config.user,
-            property:   config.property,
-            trackingType,
-            clientId:   getClientID(),
-            merchantId: getMerchantID()
-        };
+    const data = {
+        ...trackingData,
+        user:       config.user,
+        property:   config.property,
+        trackingType,
+        clientId:   getClientID(),
+        merchantId: getMerchantID()
+    };
 
-        // paramsToBeaconUrl is a function that gives you the ability to override the beacon url
-        // to whatever you want it to be based on the trackingType string and data object.
-        // This can be useful for testing purposes, this feature won't be used by merchants.
-        if (config.paramsToBeaconUrl) {
-            img.src = config.paramsToBeaconUrl({ trackingType, data });
-        } else {
-            img.src = `https://www.paypal.com/targeting/track/${ trackingType }?data=${ encodeData(data) }`;
-        }
+    // paramsToBeaconUrl is a function that gives you the ability to override the beacon url
+    // to whatever you want it to be based on the trackingType string and data object.
+    // This can be useful for testing purposes, this feature won't be used by merchants.
+    if (config.paramsToBeaconUrl) {
+        img.src = config.paramsToBeaconUrl({ trackingType, data });
+    } else {
+        img.src = `https://www.paypal.com/targeting/track/${ trackingType }?data=${ encodeData(data) }`;
+    }
 
-        img.addEventListener('load', () => resolve());
-        img.addEventListener('error', reject);
-        if (document.body) {
-            document.body.appendChild(img);
-        }
-    });
+    if (document.body) {
+        document.body.appendChild(img);
+    }
 };
 
-const trackCartEvent = <T>(config : Config, cartEventType : CartEventType, trackingData : T) : Promise<void> =>
+const trackCartEvent = <T>(config : Config, cartEventType : CartEventType, trackingData : T) =>
     track(config, 'cartEvent', { ...trackingData, cartEventType });
 
 const generateId = () : string => Math.random().toString(16).slice(2);
