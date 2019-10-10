5b969870dc3527c0f5d5aaa75863589474be5be7
Song Zheng <sozheng@paypal.com>
2019-10-10 15:02:19 -0700

    robust error logger

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,15 +1,35 @@
 /* @flow */
 import { getLogger } from '@paypal/sdk-client/src/logger';
+import { noop, stringifyError, stringifyErrorMessage } from 'belter/src';
 import { FPTI_KEY } from '@paypal/sdk-constants/src';
 
 export const logger = {
-  error: (...args) => {
-    const loggerObj = getLogger();
-    loggerObj.track({
-      [FPTI_KEY.ERROR_CODE]: 'paypal-muse-components',
-      [FPTI_KEY.ERROR_DESC]: args[0] || 'sdk-client-error'
-    });
-    loggerObj.error(...args);
+  error: (name : string, errInfo : Object) => {
+    try {
+      const loggerObj = getLogger();
+      loggerObj.track({
+        [FPTI_KEY.ERROR_CODE]: 'paypal-muse-components',
+        [FPTI_KEY.ERROR_DESC]: JSON.stringify({
+          name,
+          error: stringifyErrorMessage(errInfo)
+        })
+      });
+      loggerObj.error(name, stringifyError(errInfo));
+      loggerObj.flush().catch(noop); // fire immediately
+    } catch (err) {
+
+      // Should not deviate from logger.js
+      // https://github.com/paypal/paypal-sdk-client/blob/1da04d91df71851d1ff3a15ece659d3d944e4f3f/src/logger.js
+      const loggerObj = getLogger();
+      loggerObj.track({
+        [FPTI_KEY.ERROR_CODE]: 'paypal-muse-components',
+        [FPTI_KEY.ERROR_DESC]: `Error logging error event for ${ name || '' }`
+      });
+      loggerObj.error('logger_error', {
+        err: stringifyError(err)
+      });
+      loggerObj.flush().catch(noop); // fire immediately
+    }
   }
 };
 
