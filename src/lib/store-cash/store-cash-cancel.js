aed5ac7a89e75bb160fce63b2da79f1ba0a9621a
M-OC <mkoconnell@paypal.com>
2019-10-15 15:11:06 -0700

    storecash cancel events fire

--- src/lib/store-cash/store-cash-cancel.js
+++ src/lib/store-cash/store-cash-cancel.js
@@ -1,6 +1,17 @@
 /* @flow */
 import type { Config } from '../../types';
 
+import { storeCashFpti } from './store-cash-fpti';
+
 export const storeCashCancel = (config : Config) => {
-  
+  storeCashFpti(config, {
+    fltp: 'analytics',
+    es: 'merchantRecognizedUser',
+    mru: true,
+    website: 'muse',
+    feature: 'offer',
+    subfeature1: 'store-cash',
+    subfeature2: 'sdk',
+    flavor: 'merchantRecognizedUser'
+  });
 };
