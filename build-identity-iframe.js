4bd42f4644eaa5c331ca58f9f3490503a9717a61
M-OC <mkoconnell@paypal.com>
2019-11-05 08:46:58 -0800

    iframe points to different endpoints based on environment

--- build-identity-iframe.js
+++ build-identity-iframe.js
@@ -1,7 +1,44 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const babelConfig = require('./babel.config.js')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 const path = require('path')
+
+let acceptedEnvironments = ['production', 'staging', 'development', 'sandbox']
+let environment = process.env.NODE_ENV
+
+if (!acceptedEnvironments.includes(environment)) {
+  throw new Error(`Environment ${environment} not supported`)
+}
+
+const babelConfig = {
+  'presets': [
+    [
+      '@babel/env', {
+        'targets': {
+          'ie': 11,
+          'chrome': 27,
+          'firefox': 30,
+          'safari': 7,
+          'opera': 23
+        },
+        'loose': true,
+        'exclude': [
+          'transform-regenerator'
+        ]
+      }
+    ],
+    '@babel/react',
+    '@babel/flow'
+  ],
+  'plugins': [
+    'transform-inline-environment-variables',
+    'babel-plugin-transform-es2015-modules-commonjs',
+    [ '@babel/plugin-syntax-dynamic-import', { 'loose': true } ],
+    [ '@babel/plugin-proposal-decorators', { 'loose': true, 'legacy': true } ],
+    [ '@babel/plugin-proposal-class-properties', { 'loose': true } ],
+    [ '@babel/plugin-transform-for-of', { 'assumeArray': true } ],
+    [ '@babel/plugin-transform-runtime', { 'corejs': false, 'helpers': true, 'regenerator': false } ]
+  ]
+}
 
 const webpackConfig = {
   mode: 'production',
--- iframes/identity/identity.js
+++ iframes/identity/identity.js
@@ -1,55 +1,71 @@
-  const fetchVisitorInfo = (data) => {
-    const deviceInfo = encodeURIComponent(JSON.stringify(data.deviceInfo))
-    const country = data.country
+const env = process.env.NODE_ENV
+let targetingUrl
 
-    const fetchOptions = {
-      method: 'POST',
-      credentials: 'include',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        query: `{ visitorInfo(
-          country: "${country}",
-          deviceInfo: "${deviceInfo}"
-        ) }`
-      })
-    }
+switch (env) {
+  case 'staging':
+  case 'development':
+    targetingUrl = 'https://www.msmaster.qa.paypal.com/targeting/qraphql'
+    break;
+  case 'sandbox':
+    targetingUrl = 'https://www.sandbox.paypal.com/targeting/graphql'
+  case 'production':
+  default:
+    targetingUrl = 'https://www.paypal.com/targeting/graphql'
+    break;
+}
 
-    return fetch('https://www.paypal.com/targeting/graphql', fetchOptions)
-      .then(res => {
-        if (res.status !== 200) {
-          throw new Error(`targeting responded with statuscode ${res.status}`)
-        }
+const fetchVisitorInfo = (data) => {
+  const deviceInfo = encodeURIComponent(JSON.stringify(data.deviceInfo))
+  const country = data.country
 
-        return res.json()
-      })
-      .then(json => {
-        if (!json || !json.data || !json.data.visitorInfo) {
-          throw new Error('response missing required fields')
-        }
-
-        return json.data.visitorInfo
-      })
+  const fetchOptions = {
+    method: 'POST',
+    credentials: 'include',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      query: `{ visitorInfo(
+        country: "${country}",
+        deviceInfo: "${deviceInfo}"
+      ) }`
+    })
   }
 
-  window.addEventListener('message', async (e) => {
-    if (e.source.window !== window.parent.window) {
-      return
-    }
+  return fetch(targetingUrl, fetchOptions)
+    .then(res => {
+      if (res.status !== 200) {
+        throw new Error(`targeting responded with statuscode ${res.status}`)
+      }
 
-    if (e.data.type !== 'fetch_identity_request') {
-      return
-    }
+      return res.json()
+    })
+    .then(json => {
+      if (!json || !json.data || !json.data.visitorInfo) {
+        throw new Error('response missing required fields')
+      }
 
-    try {
-      const visitorInfo = await fetchVisitorInfo(e.data.payload)
-      window.parent.postMessage({
-        type: 'fetch_identity_response',
-        payload: visitorInfo
-      }, '*')
-    } catch (err) {
-      //surface error in some way
-    }
-  })
+      return json.data.visitorInfo
+    })
+}
+
+window.addEventListener('message', async (e) => {
+  if (e.source.window !== window.parent.window) {
+    return
+  }
+
+  if (e.data.type !== 'fetch_identity_request') {
+    return
+  }
+
+  try {
+    const visitorInfo = await fetchVisitorInfo(e.data.payload)
+    window.parent.postMessage({
+      type: 'fetch_identity_response',
+      payload: visitorInfo
+    }, '*')
+  } catch (err) {
+    //surface error in some way
+  }
+})
