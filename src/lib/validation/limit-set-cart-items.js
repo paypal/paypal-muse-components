a4dfeccad721f270f332015c550792d6e112e241
Christine <chrsun@paypal.com>
2019-11-15 17:13:51 -0800

    feat(limitsetcart): remove total calc

--- src/lib/validation/limit-set-cart-items.js
+++ src/lib/validation/limit-set-cart-items.js
@@ -1,10 +1,5 @@
 /* @flow */
 export const limitSetCartItems = (input : any)  => {
   input.items = input.items.splice(0, 10);
-  if (input.cartTotal) {
-    input.cartTotal = input.items.length;
-  }  else if (input.total) {
-    input.total = input.items.length;
-  }
   return input;
 };
