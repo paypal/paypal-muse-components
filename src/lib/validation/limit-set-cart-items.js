6e0875753e4018d012586a1a598e21e7829e172a
Christine <chrsun@paypal.com>
2019-11-15 16:55:34 -0800

    feat(limitsetcart): fix set total

--- src/lib/validation/limit-set-cart-items.js
+++ src/lib/validation/limit-set-cart-items.js
@@ -1,6 +1,10 @@
 /* @flow */
 export const limitSetCartItems = (input : any)  => {
-  input.items = input.items.splice(0,10);
-  input.cartTotal = input.items.length;
+  input.items = input.items.splice(0, 10);
+  if (input.cartTotal) {
+    input.cartTotal = input.items.length;
+  }  else if (input.total) {
+    input.total = input.items.length;
+  }
   return input;
 };
