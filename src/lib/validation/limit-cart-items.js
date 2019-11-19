ba6f54fc6b5faa8867ab0d7b6537ec387ad6db60
Christine <chrsun@paypal.com>
2019-11-19 10:46:22 -0800

    feat(limitsetcart): address comments

--- src/lib/validation/limit-cart-items.js
+++ src/lib/validation/limit-cart-items.js
@@ -3,9 +3,9 @@ import constants from '../constants';
 
 export const limitCartItems = (input : any)  => {
   const { cartLimit } = constants;
-  if (input.items.length > 10) {
+  if (input.items.length > cartLimit) {
     // eslint-disable-next-line no-console
-    console.warn('Total number of items in cart is greater than 10, cart will be truncated');
+    console.warn(`Total number of items in cart is greater than ${cartLimit}, cart will be truncated`);
   }
   input.items = input.items.splice(0, cartLimit);
   return input;
