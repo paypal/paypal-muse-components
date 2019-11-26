f8924ae7b0b02b15e46ca1bea6187b5e6c8ba529
Christine <chrsun@paypal.com>
2019-11-26 13:30:15 -0800

    feat(limitsetcart): remove logger

--- src/lib/validation/limit-cart-items.js
+++ src/lib/validation/limit-cart-items.js
@@ -5,7 +5,6 @@ import { logger } from '../logger';
 export const limitCartItems = (input : any)  => {
   const { cartLimit } = constants;
   if (input.items && input.items.length > cartLimit) {
-    logger.error(`Total number of items in cart is greater than ${ cartLimit }, cart will be truncated`);
     input.items = input.items.splice(0, cartLimit);
   }
   return input;
