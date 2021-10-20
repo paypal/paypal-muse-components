5808adc6cfe13ad36aee6320b2e22b0fab8870d0
zbhill <zbhill@paypal.com>
2021-10-20 11:39:50 -0500

    DTSHOPSDK-622: fix failing build due to require and console lint

--- test-site-server.js
+++ test-site-server.js
@@ -10,5 +10,5 @@ const PORT = process.env.PORT || 3008;
 app.use('/paypal-muse-components', express.static('docs'));
 
 app.listen(PORT, () => {
-  console.log(`test server running on port ${PORT}`);
-})
+  console.log(`Test site running on http://localhost:${PORT}/paypal-muse-components/`);
+});
