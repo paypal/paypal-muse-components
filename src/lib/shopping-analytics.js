8e0bd86f2fdcde607fc8269c2819bafac3f08297
Yehor Anisimov <yanisimov@paypal.com>
2025-12-17 17:47:25 -0500

    DTSHOPSCE-7414: Remove shopping analytics (store-cash)

--- src/lib/shopping-analytics.js
+++ src/lib/shopping-analytics.js
@@ -1,84 +1,13 @@
 /* @flow */
-// $FlowFixMe
-import 'whatwg-fetch'; // eslint-disable-line import/no-unassigned-import
+const noop = () => {};
 
-import type { Config } from '../types/config';
-import type { ContainerSummary } from '../types';
-import type { VisitorInfo } from '../types/user';
-
-import { setupTrackers } from './shopping-trackers';
-import { setupUserDetails } from './user-configuration';
-import { setupContainer } from './get-property-id';
-// $FlowFixMe
-import { capturePageData } from './tag-parsers/capture-page-data';
-import { debugLogger } from './debug-console-logger';
-
-// $FlowFixMe
-export const shoppingAnalyticsSetup = (config : Config = {}) => {
-  const shoppingTracker = setupTrackers(config);
-  let identityFetchCompleted : boolean = false;
-  let containerFetchCompleted : boolean = false;
-  let eventQueue = [];
-  let inMemoryIdentity: VisitorInfo = null;
-  const queue_limit = 100;
-
-  function isReadyToPublish() : boolean {
-    return identityFetchCompleted && containerFetchCompleted;
-  }
-
-  function flushEventQueueIfReady () {
-    if (isReadyToPublish()) {
-      for (const params of eventQueue) {
-        shoppingTracker.send(...params, inMemoryIdentity);
-      }
-      eventQueue = [];
-    }
-  }
-
-  function onUserIdentityFetch(identity: VisitorInfo) {
-    inMemoryIdentity = identity;
-    debugLogger.log('[shopping-analytics:onUserIdentityFetch] Received identity fetch notification.');
-    identityFetchCompleted = true;
-    flushEventQueueIfReady();
-  }
-
-  // $FlowFixMe
-  function onContainerFetch(containerSummary : ContainerSummary) : void  {
-    debugLogger.log('[shopping-analytics:onUserIdentityFetch] Received container fetch notification. Container summary: ', containerSummary);
-    config.propertyId = config.propertyId || (containerSummary && containerSummary.id);
-    config.containerSummary = containerSummary;
-    containerFetchCompleted = true;
-    flushEventQueueIfReady();
-  }
-
-  function enqueueEvent(...args) {
-    debugLogger.log('[shopping-analytics:enqueueEvent] Event is enqueued (wating on initialization). Event(s):', args);
-    eventQueue.push(args);
-    if (eventQueue.length > queue_limit) {
-      eventQueue.shift();
-    }
-  }
-
-  // $FlowFixMe
-  function sendOrEnqueue(...args) {
-    if (!isReadyToPublish()) {
-      enqueueEvent(...args);
-    } else {
-      shoppingTracker.send(...args);
-    }
-  }
-  
-  debugLogger.log('[shopping-analytics:shoppingAnalyticsSetup] Initialized shopping analytics with the following configuration', config);
-
-  setupUserDetails(config, onUserIdentityFetch);
-  setupContainer(config, onContainerFetch);
-
+export const shoppingAnalyticsSetup = () => {
   // return publicly accessible
   return {
-    send: sendOrEnqueue,
-    set: shoppingTracker.set,
-    getPageSkuData: capturePageData,
-    enableDebugLogging: () => { debugLogger.setDebugEnabled(true); },
-    disableDebugLogging: () => { debugLogger.setDebugEnabled(false); }
+    send: noop,
+    set: noop,
+    getPageSkuData: noop,
+    enableDebugLogging: noop,
+    disableDebugLogging: noop
   };
 };
--- src/shopping-component.js
+++ src/shopping-component.js
@@ -1,42 +1,13 @@
 /* @flow */
-// $FlowFixMe
-import 'whatwg-fetch'; // eslint-disable-line import/no-unassigned-import
-
-import constants from './lib/constants';
 import { shoppingAnalyticsSetup } from './lib/shopping-analytics';
-import { setupShoppingDDL, DDL_NAME } from './lib/ddl/shopping-ddl';
-import type { Config } from './types';
 
-const { defaultTrackerConfig } = constants;
-
-/**
- *  Parse merchant provided config and cache the user details passed
- *  in by the merchant if any as well as try to identify the user by calling VPNS.
- *
- * Further, fetch and cache the container associated with the propertyId or the url.
- * @param config
- * @returns {{viewPage: function(Object): void}}
- * @constructor
- */
 // $FlowFixMe
-export const ShoppingAnalytics = (config : Config = {}) => {
-  // $FlowFixMe
-  config = { ...defaultTrackerConfig, ...config };
-  const shoppingAnalytics = shoppingAnalyticsSetup(config);
+export const ShoppingAnalytics = () => {
+  const shoppingAnalytics = shoppingAnalyticsSetup();
   window.__pp__trackers__ = window.__pp__trackers__ || [];
   window.__pp__trackers__.push(shoppingAnalytics);
 
   return shoppingAnalytics;
 };
 
-function init() {
-  if (window[DDL_NAME]) {
-    const analytics = new ShoppingAnalytics();
-    setupShoppingDDL(window, analytics);
-  }
-}
-
-
-export function setup() {
-  init();
-}
+export function setup() {}
